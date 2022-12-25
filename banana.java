729dff4ab6f87fd30facc6cf9f627777abc0d2c8
Mensun Wang <mensunw@gmail.com>
2022-12-25 14:51:53 -0500

    first commit

---
+++ banana.java
@@ -0,0 +1,8 @@
+public class banana{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        //Call code to take in input
+        //Calculate using input
+        
+    }
+}
