58f4a3168dd657672e1d802a54be3fe27d4ec70d
Mensun Wang <mensunw@gmail.com>
2022-12-27 21:47:44 -0500

    added some function init

---
+++ calculations.java
@@ -0,0 +1,35 @@
+public class calculations {
+    //main driver
+    //need while loop for each balloon popped and check what can be bought within there
+
+    //use tree structure to go through each possibility
+
+    public static void driver(){
+        
+    }
+
+    //each time money comes in, it's a loop
+    public static void money_gameLoop(){
+    //if end of round, then round_end();
+        if(is_round_end()){
+
+        }
+        balloonPopped();
+        
+
+    }
+
+    //dont think is needed
+    public static void game_round(){
+
+    }
+
+    //given a tower, it will set money to
+    public static void tower_possibilities(){
+
+    }
+
+    public static boolean is_round_end(){
+
+    }
+}
